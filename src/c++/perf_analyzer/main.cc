ec6454f69e0404304fe4777a1442c15cbf61e082
Matthew Kotila <matthew.r.kotila@gmail.com>
2022-02-01 22:03:32 +0000

    Addressed comments

--- src/c++/perf_analyzer/main.cc
+++ src/c++/perf_analyzer/main.cc
@@ -27,13 +27,9 @@
 
 #include <getopt.h>
 #include <signal.h>
-
 #include <algorithm>
-
 #include "concurrency_manager.h"
 #include "custom_load_manager.h"
-#include "grpc_client.h"
-#include "http_client.h"
 #include "inference_profiler.h"
 #include "model_parser.h"
 #include "perf_utils.h"
@@ -272,16 +268,17 @@ Usage(char** argv, const std::string& msg = std::string())
   std::cerr << "\t-u <URL for inference service>" << std::endl;
   std::cerr << "\t-i <Protocol used to communicate with inference service>"
             << std::endl;
+  std::cerr << "\t--ssl-grpc-use-ssl <bool>" << std::endl;
   std::cerr << "\t--ssl-grpc-root-certifications-file <path>" << std::endl;
   std::cerr << "\t--ssl-grpc-private-key-file <path>" << std::endl;
   std::cerr << "\t--ssl-grpc-certificate-chain-file <path>" << std::endl;
-  std::cerr << "\t--ssl-http-verify-peer <number>" << std::endl;
-  std::cerr << "\t--ssl-http-verify-host <number>" << std::endl;
-  std::cerr << "\t--ssl-http-ca-certificates-file <path>" << std::endl;
-  std::cerr << "\t--ssl-http-client-certificate-file <path>" << std::endl;
-  std::cerr << "\t--ssl-http-client-certificate-type <string>" << std::endl;
-  std::cerr << "\t--ssl-http-private-key-file <path>" << std::endl;
-  std::cerr << "\t--ssl-http-private-key-type <string>" << std::endl;
+  std::cerr << "\t--ssl-https-verify-peer <number>" << std::endl;
+  std::cerr << "\t--ssl-https-verify-host <number>" << std::endl;
+  std::cerr << "\t--ssl-https-ca-certificates-file <path>" << std::endl;
+  std::cerr << "\t--ssl-https-client-certificate-file <path>" << std::endl;
+  std::cerr << "\t--ssl-https-client-certificate-type <string>" << std::endl;
+  std::cerr << "\t--ssl-https-private-key-file <path>" << std::endl;
+  std::cerr << "\t--ssl-https-private-key-type <string>" << std::endl;
   std::cerr << std::endl;
   std::cerr << "IV. OTHER OPTIONS: " << std::endl;
   std::cerr << "\t-f <filename for storing report in csv format>" << std::endl;
@@ -618,6 +615,12 @@ Usage(char** argv, const std::string& msg = std::string())
                    "are gRPC and HTTP. Default is HTTP.",
                    38)
             << std::endl;
+  std::cerr << std::setw(38) << std::left << " --ssl-grpc-use-ssl: "
+            << FormatMessage(
+                   "Bool (true|false) for whether to use encrypted channel to "
+                   "the server. Default false.",
+                   38)
+            << std::endl;
   std::cerr << std::setw(38) << std::left
             << " --ssl-grpc-root-certifications-file: "
             << FormatMessage(
@@ -638,7 +641,7 @@ Usage(char** argv, const std::string& msg = std::string())
                    "certificate chain.",
                    38)
             << std::endl;
-  std::cerr << std::setw(38) << std::left << " --ssl-http-verify-peer: "
+  std::cerr << std::setw(38) << std::left << " --ssl-https-verify-peer: "
             << FormatMessage(
                    "Number (0|1) to verify the peer's SSL certificate. See "
                    "https://curl.se/libcurl/c/CURLOPT_SSL_VERIFYPEER.html for "
@@ -646,7 +649,7 @@ Usage(char** argv, const std::string& msg = std::string())
                    38)
             << std::endl;
   std::cerr
-      << std::setw(38) << std::left << " --ssl-http-verify-host: "
+      << std::setw(38) << std::left << " --ssl-https-verify-host: "
       << FormatMessage(
              "Number (0|1|2) to verify the certificate's name against host. "
              "See https://curl.se/libcurl/c/CURLOPT_SSL_VERIFYHOST.html for "
@@ -654,25 +657,25 @@ Usage(char** argv, const std::string& msg = std::string())
              38)
       << std::endl;
   std::cerr << std::setw(38) << std::left
-            << " --ssl-http-ca-certificates-file: "
+            << " --ssl-https-ca-certificates-file: "
             << FormatMessage("Path to Certificate Authority (CA) bundle.", 38)
             << std::endl;
   std::cerr << std::setw(38) << std::left
-            << " --ssl-http-client-certificate-file: "
+            << " --ssl-https-client-certificate-file: "
             << FormatMessage("Path to the SSL client certificate.", 38)
             << std::endl;
   std::cerr
-      << std::setw(38) << std::left << " --ssl-http-client-certificate-type: "
+      << std::setw(38) << std::left << " --ssl-https-client-certificate-type: "
       << FormatMessage(
              "Type (PEM|DER) of the client SSL certificate. Default is PEM.",
              38)
       << std::endl;
-  std::cerr << std::setw(38) << std::left << " --ssl-http-private-key-file: "
+  std::cerr << std::setw(38) << std::left << " --ssl-https-private-key-file: "
             << FormatMessage(
                    "Path to the private keyfile for TLS and SSL client cert.",
                    38)
             << std::endl;
-  std::cerr << std::setw(38) << std::left << " --ssl-http-private-key-type: "
+  std::cerr << std::setw(38) << std::left << " --ssl-https-private-key-type: "
             << FormatMessage(
                    "Type (PEM|DER) of the private key file. Default is PEM.",
                    38)
@@ -796,18 +799,7 @@ main(int argc, char** argv)
   std::string memory_type = DEFAULT_MEMORY_TYPE;  // currently not used
 
   // gRPC and HTTP SSL options
-  std::string ssl_grpc_root_certifications_file = "";
-  std::string ssl_grpc_private_key_file = "";
-  std::string ssl_grpc_certificate_chain_file = "";
-  long ssl_http_verify_peer = 1L;
-  long ssl_http_verify_host = 2L;
-  std::string ssl_http_ca_certificates_file = "";
-  std::string ssl_http_client_certificate_file = "";
-  triton::client::HttpSslOptions::CERTTYPE ssl_http_client_certificate_type =
-      triton::client::HttpSslOptions::CERTTYPE::CERT_PEM;
-  std::string ssl_http_private_key_file = "";
-  triton::client::HttpSslOptions::KEYTYPE ssl_http_private_key_type =
-      triton::client::HttpSslOptions::KEYTYPE::KEY_PEM;
+  struct cb::SslOptionsBase ssl_options;
 
   // {name, has_arg, *flag, val}
   static struct option long_options[] = {
@@ -842,16 +834,17 @@ main(int argc, char** argv)
       {"triton-server-directory", 1, 0, 28},
       {"model-repository", 1, 0, 29},
       {"sequence-id-range", 1, 0, 30},
-      {"ssl-grpc-root-certifications-file", 1, 0, 31},
-      {"ssl-grpc-private-key-file", 1, 0, 32},
-      {"ssl-grpc-certificate-chain-file", 1, 0, 33},
-      {"ssl-http-verify-peer", 1, 0, 34},
-      {"ssl-http-verify-host", 1, 0, 35},
-      {"ssl-http-ca-certificates-file", 1, 0, 36},
-      {"ssl-http-client-certificate-file", 1, 0, 37},
-      {"ssl-http-client-certificate-type", 1, 0, 38},
-      {"ssl-http-private-key-file", 1, 0, 39},
-      {"ssl-http-private-key-type", 1, 0, 40},
+      {"ssl-grpc-use-ssl", 1, 0, 31},
+      {"ssl-grpc-root-certifications-file", 1, 0, 32},
+      {"ssl-grpc-private-key-file", 1, 0, 33},
+      {"ssl-grpc-certificate-chain-file", 1, 0, 34},
+      {"ssl-https-verify-peer", 1, 0, 35},
+      {"ssl-https-verify-host", 1, 0, 36},
+      {"ssl-https-ca-certificates-file", 1, 0, 37},
+      {"ssl-https-client-certificate-file", 1, 0, 38},
+      {"ssl-https-client-certificate-type", 1, 0, 39},
+      {"ssl-https-private-key-file", 1, 0, 40},
+      {"ssl-https-private-key-type", 1, 0, 41},
       {0, 0, 0, 0}};
 
   // Parse commandline...
@@ -1152,8 +1145,22 @@ main(int argc, char** argv)
         break;
       }
       case 31: {
+        std::string arg = optarg;
+        std::transform(
+            arg.begin(), arg.end(), arg.begin(),
+            [](unsigned char c) { return std::tolower(c); });
+        if (arg == "true") {
+          ssl_options.ssl_grpc_use_ssl = true;
+        } else if (arg == "false") {
+          ssl_options.ssl_grpc_use_ssl = false;
+        } else {
+          Usage(argv, "--ssl-grpc-use-ssl must be 'true' or 'false'");
+        }
+        break;
+      }
+      case 32: {
         if (pa::IsFile(optarg)) {
-          ssl_grpc_root_certifications_file = optarg;
+          ssl_options.ssl_grpc_root_certifications_file = optarg;
         } else {
           Usage(
               argv,
@@ -1161,17 +1168,17 @@ main(int argc, char** argv)
         }
         break;
       }
-      case 32: {
+      case 33: {
         if (pa::IsFile(optarg)) {
-          ssl_grpc_private_key_file = optarg;
+          ssl_options.ssl_grpc_private_key_file = optarg;
         } else {
           Usage(argv, "--ssl-grpc-private-key-file must be a valid file path");
         }
         break;
       }
-      case 33: {
+      case 34: {
         if (pa::IsFile(optarg)) {
-          ssl_grpc_certificate_chain_file = optarg;
+          ssl_options.ssl_grpc_certificate_chain_file = optarg;
         } else {
           Usage(
               argv,
@@ -1179,74 +1186,66 @@ main(int argc, char** argv)
         }
         break;
       }
-      case 34: {
-        if (std::atol(optarg) == 0 || std::atol(optarg) == 1) {
-          ssl_http_verify_peer = std::atol(optarg);
-        } else {
-          Usage(argv, "--ssl-http-verify-peer must be 0 or 1");
-        }
-        break;
-      }
       case 35: {
-        if (std::atol(optarg) == 0 || std::atol(optarg) == 1 ||
-            std::atol(optarg) == 2) {
-          ssl_http_verify_host = std::atol(optarg);
+        if (std::atol(optarg) == 0 || std::atol(optarg) == 1) {
+          ssl_options.ssl_https_verify_peer = std::atol(optarg);
         } else {
-          Usage(argv, "--ssl-http-verify-host must be 0, 1, or 2");
+          Usage(argv, "--ssl-https-verify-peer must be 0 or 1");
         }
         break;
       }
       case 36: {
-        if (pa::IsFile(optarg)) {
-          ssl_http_ca_certificates_file = optarg;
+        if (std::atol(optarg) == 0 || std::atol(optarg) == 1 ||
+            std::atol(optarg) == 2) {
+          ssl_options.ssl_https_verify_host = std::atol(optarg);
         } else {
-          Usage(
-              argv,
-              "--ssl-http-ca-certificates-file must be a valid file path");
+          Usage(argv, "--ssl-https-verify-host must be 0, 1, or 2");
         }
         break;
       }
       case 37: {
         if (pa::IsFile(optarg)) {
-          ssl_http_client_certificate_file = optarg;
+          ssl_options.ssl_https_ca_certificates_file = optarg;
         } else {
           Usage(
               argv,
-              "--ssl-http-client-certificate-file must be a valid file path");
+              "--ssl-https-ca-certificates-file must be a valid file path");
         }
         break;
       }
       case 38: {
-        if (std::string(optarg) == "PEM") {
-          ssl_http_client_certificate_type =
-              triton::client::HttpSslOptions::CERTTYPE::CERT_PEM;
-        } else if (std::string(optarg) == "DER") {
-          ssl_http_client_certificate_type =
-              triton::client::HttpSslOptions::CERTTYPE::CERT_DER;
+        if (pa::IsFile(optarg)) {
+          ssl_options.ssl_https_client_certificate_file = optarg;
         } else {
           Usage(
               argv,
-              "--ssl-http-client-certificate-type must be 'PEM' or 'DER'");
+              "--ssl-https-client-certificate-file must be a valid file path");
         }
         break;
       }
       case 39: {
-        if (pa::IsFile(optarg)) {
-          ssl_http_private_key_file = optarg;
+        if (std::string(optarg) == "PEM" || std::string(optarg) == "DER") {
+          ssl_options.ssl_https_client_certificate_type = optarg;
         } else {
-          Usage(argv, "--ssl-http-private-key-file must be a valid file path");
+          Usage(
+              argv,
+              "--ssl-https-client-certificate-type must be 'PEM' or 'DER'");
         }
         break;
       }
       case 40: {
-        if (std::string(optarg) == "PEM") {
-          ssl_http_private_key_type =
-              triton::client::HttpSslOptions::KEYTYPE::KEY_PEM;
-        } else if (std::string(optarg) == "DER") {
-          ssl_http_private_key_type =
-              triton::client::HttpSslOptions::KEYTYPE::KEY_DER;
+        if (pa::IsFile(optarg)) {
+          ssl_options.ssl_https_private_key_file = optarg;
         } else {
-          Usage(argv, "--ssl-http-private-key-type must be 'PEM' or 'DER'");
+          Usage(argv, "--ssl-https-private-key-file must be a valid file path");
+        }
+        break;
+      }
+      case 41: {
+        if (std::string(optarg) == "PEM" || std::string(optarg) == "DER") {
+          ssl_options.ssl_https_private_key_type = optarg;
+        } else {
+          Usage(argv, "--ssl-https-private-key-type must be 'PEM' or 'DER'");
         }
         break;
       }
