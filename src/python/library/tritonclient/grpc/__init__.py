dca19ab3214b735038390a69222b804dbf834870
RRoundTable <roundtable@annotation-ai.com>
2022-11-13 21:05:56 +0900

    Edit method name

--- src/python/library/tritonclient/grpc/__init__.py
+++ src/python/library/tritonclient/grpc/__init__.py
@@ -1852,13 +1852,13 @@ class InferInput:
         else:
             self._raw_content = input_tensor.tobytes()
 
-    def set_data_from_numpy(self, raw_content):
+    def set_data_from_bytes(self, raw_content):
         """Set the tensor data from the specified bytes for
         input associated with this object.
 
         Parameters
         ----------
-        input_tensor : bytes
+        raw_content : bytes
             The tensor data in bytes format
         """
         self._raw_content = raw_content
