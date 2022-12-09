4bb65af5c8affadeee954e358ebead72717aebe2
Timothy Gerdes <50968584+tgerdesnv@users.noreply.github.com>
2022-12-09 07:47:42 -0600

    GetAccumulatedClientStat should discard any existing data in the input arg (#209)

--- src/c++/perf_analyzer/test_load_manager.cc
+++ src/c++/perf_analyzer/test_load_manager.cc
@@ -217,31 +217,6 @@ class TestLoadManager : public TestLoadManagerBase, public LoadManager {
       CHECK(result_stat.cumulative_receive_time_ns == 5);
       CHECK(ret.IsOk() == true);
     }
-    SUBCASE("Existing data in function arg")
-    {
-      // If the input InferStat already has data in it,
-      // it will be included in the output result
-      //
-      result_stat.completed_request_count = 10;
-      result_stat.cumulative_total_request_time_ns = 10;
-      result_stat.cumulative_send_time_ns = 10;
-      result_stat.cumulative_receive_time_ns = 10;
-
-      auto stat1 = std::make_shared<ThreadStat>();
-      stat1->contexts_stat_.push_back(cb::InferStat());
-      stat1->contexts_stat_[0].completed_request_count = 2;
-      stat1->contexts_stat_[0].cumulative_total_request_time_ns = 3;
-      stat1->contexts_stat_[0].cumulative_send_time_ns = 4;
-      stat1->contexts_stat_[0].cumulative_receive_time_ns = 5;
-      threads_stat_.push_back(stat1);
-
-      auto ret = GetAccumulatedClientStat(&result_stat);
-      CHECK(result_stat.completed_request_count == 12);
-      CHECK(result_stat.cumulative_total_request_time_ns == 13);
-      CHECK(result_stat.cumulative_send_time_ns == 14);
-      CHECK(result_stat.cumulative_receive_time_ns == 15);
-      CHECK(ret.IsOk() == true);
-    }
     SUBCASE("Multiple thread multiple contexts")
     {
       auto stat1 = std::make_shared<ThreadStat>();
