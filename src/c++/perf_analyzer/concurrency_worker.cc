c2a599059f669a3a61823c1927af5015aef9e493
tgerdes <tgerdes@nvidia.com>
2023-01-24 17:23:54 -0600

    Fix free_ctx_id tracking, and decouple total_ongoing_request tracking

--- src/c++/perf_analyzer/concurrency_worker.cc
+++ src/c++/perf_analyzer/concurrency_worker.cc
@@ -149,8 +149,8 @@ ConcurrencyWorker::PrepAndSendInferRequests()
   // matches the concurrency level
   // Non-sequence model is 'num_reqs' * 1 ctx
   // Sequence model is 1 request of 1 sequence * 'active_ctx_cnt' ctxs_
-  while (total_ongoing_requests_ < (int)thread_config_->concurrency_ &&
-         early_exit == false && execute_ && thread_stat_->status_.IsOk()) {
+  while (free_ctx_ids_.size() && early_exit == false && execute_ &&
+         thread_stat_->status_.IsOk()) {
     uint32_t ctx_id = GetCtxId();
     PrepAndSendInferRequest(ctx_id);
     RestoreFreeCtxId(ctx_id);
@@ -160,7 +160,6 @@ ConcurrencyWorker::PrepAndSendInferRequests()
 void
 ConcurrencyWorker::RestoreFreeCtxId(uint32_t ctx_id)
 {
-  // FIXME TMA-1023 we are clearly pushing and not popping in some cases
   if (!async_) {
     {
       std::lock_guard<std::mutex> lock(cb_mtx_);
@@ -197,8 +196,6 @@ ConcurrencyWorker::AsyncCallbackFinalize(uint32_t ctx_id)
     notified_ = true;
   }
 
-  total_ongoing_requests_--;
-
   cb_cv_.notify_all();
 }
 
@@ -229,8 +226,15 @@ ConcurrencyWorker::ResetFreeCtxIds()
 {
   std::lock_guard<std::mutex> lock(cb_mtx_);
   free_ctx_ids_ = std::queue<int>();
-  for (size_t i = 0; i < ctxs_.size(); ++i) {
-    free_ctx_ids_.push(i);
+
+  if (ctxs_.size() > 1) {
+    for (size_t i = 0; i < ctxs_.size(); ++i) {
+      free_ctx_ids_.push(i);
+    }
+  } else {
+    for (size_t i = 0; i < thread_config_->concurrency_; i++) {
+      free_ctx_ids_.push(0);
+    }
   }
 }
 
@@ -249,19 +253,14 @@ uint32_t
 ConcurrencyWorker::GetCtxId()
 {
   uint32_t ctx_id;
-  if (on_sequence_model_) {
-    // Find the next available context id to use for this request
-    {
-      std::lock_guard<std::mutex> lk(cb_mtx_);
-
-      if (free_ctx_ids_.size() < 1) {
-        throw std::runtime_error("free ctx id list is empty");
-      }
-      ctx_id = free_ctx_ids_.front();
-      free_ctx_ids_.pop();
+  // Find the next available context id to use for this request
+  std::lock_guard<std::mutex> lk(cb_mtx_);
+  {
+    if (free_ctx_ids_.size() < 1) {
+      throw std::runtime_error("free ctx id list is empty");
     }
-  } else {
-    ctx_id = 0;
+    ctx_id = free_ctx_ids_.front();
+    free_ctx_ids_.pop();
   }
   return ctx_id;
 }
