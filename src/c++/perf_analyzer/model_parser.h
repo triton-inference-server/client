46e60de2b5c850b71ca7a781f9d24b8559b465bb
tgerdes <tgerdes@nvidia.com>
2023-03-09 07:57:45 -0600

    minor tweak

--- src/c++/perf_analyzer/model_parser.h
+++ src/c++/perf_analyzer/model_parser.h
@@ -41,7 +41,7 @@ struct ModelTensor {
   std::string name_;
   std::string datatype_;
   std::vector<int64_t> shape_;
-  // Does this tensor hold shape information for other tensors?
+  // Indicates if this tensor holds shape information for other tensors
   bool is_shape_tensor_;
   bool is_optional_;
 };
