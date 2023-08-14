f079cd904bc46fff0690709023681eace3995b2f
Hyunjae Woo <hwoo@nvidia.com>
2023-08-14 15:32:22 -0700

    Split tests into multiple test cases

--- src/c++/perf_analyzer/test_profile_data_collector.cc
+++ src/c++/perf_analyzer/test_profile_data_collector.cc
@@ -30,81 +30,98 @@
 
 namespace triton { namespace perfanalyzer {
 
-TEST_CASE("profile_data_collector")
+TEST_CASE("profile_data_collector: FindExperiment")
+{
+  MockProfileDataCollector collector{};
+  InferenceLoadMode infer_mode1{10, 20.0};
+
+  std::vector<Experiment>::iterator it;
+  it = collector.FindExperiment(infer_mode1);
+  CHECK(it == collector.experiments_.end());
+
+  std::vector<RequestRecord> request_records{RequestRecord{}};
+  collector.AddData(infer_mode1, std::move(request_records));
+
+  it = collector.FindExperiment(infer_mode1);
+  CHECK(it != collector.experiments_.end());
+  CHECK((*it).mode == infer_mode1);
+
+  InferenceLoadMode infer_mode2{123, 0.0};
+  it = collector.FindExperiment(infer_mode2);
+  CHECK(it == collector.experiments_.end());
+}
+
+TEST_CASE("profile_data_collector: AddData")
 {
   MockProfileDataCollector collector{};
   InferenceLoadMode infer_mode{10, 20.0};
 
-  std::vector<Experiment>::iterator it;
-  it = collector.FindExperiment(infer_mode);
-  CHECK(it == collector.experiments_.end());
+  // Add RequestRecords
+  auto clock_epoch{std::chrono::time_point<std::chrono::system_clock>()};
 
+  uint64_t sequence_id1{123};
+  auto request_timestamp1{clock_epoch + std::chrono::nanoseconds(1)};
+  auto response_timestamp1{clock_epoch + std::chrono::nanoseconds(2)};
+  auto response_timestamp2{clock_epoch + std::chrono::nanoseconds(3)};
 
-  SUBCASE("FindExperiment")
-  {
-    std::vector<RequestRecord> request_records{RequestRecord{}};
-    collector.AddData(infer_mode, std::move(request_records));
+  RequestRecord request_record1{
+      request_timestamp1,
+      std::vector<std::chrono::time_point<std::chrono::system_clock>>{
+          response_timestamp1, response_timestamp2},
+      0,
+      false,
+      sequence_id1,
+      false};
 
-    it = collector.FindExperiment(infer_mode);
-    CHECK(it != collector.experiments_.end());
-    CHECK((*it).mode == infer_mode);
-  }
+  uint64_t sequence_id2{456};
+  auto request_timestamp2{clock_epoch + std::chrono::nanoseconds(4)};
+  auto response_timestamp3{clock_epoch + std::chrono::nanoseconds(5)};
+  auto response_timestamp4{clock_epoch + std::chrono::nanoseconds(6)};
 
-  SUBCASE("AddData")
-  {
-    // Add RequestRecords
-    uint64_t sequence_id_0{123};
-    RequestRecord request_record_0{
-        std::chrono::system_clock::now(),
-        std::vector<std::chrono::time_point<std::chrono::system_clock>>{
-            std::chrono::system_clock::now()},
-        false,
-        false,
-        sequence_id_0,
-        false};
+  RequestRecord request_record2{
+      request_timestamp2,
+      std::vector<std::chrono::time_point<std::chrono::system_clock>>{
+          response_timestamp3, response_timestamp4},
+      0,
+      false,
+      sequence_id2,
+      false};
 
-    uint64_t sequence_id_1{456};
-    RequestRecord request_record_1{
-        std::chrono::system_clock::now(),
-        std::vector<std::chrono::time_point<std::chrono::system_clock>>{
-            std::chrono::system_clock::now()},
-        false,
-        false,
-        sequence_id_1,
-        false};
+  std::vector<RequestRecord> request_records{request_record1, request_record2};
+  collector.AddData(infer_mode, std::move(request_records));
 
-    std::vector<RequestRecord> request_records{
-        request_record_0, request_record_1};
-    collector.AddData(infer_mode, std::move(request_records));
+  CHECK(!collector.experiments_.empty());
 
-    it = collector.FindExperiment(infer_mode);
-    CHECK(it != collector.experiments_.end());
+  std::vector<RequestRecord> rr{collector.experiments_[0].requests};
+  CHECK(rr[0].sequence_id_ == sequence_id1);
+  CHECK(rr[0].start_time_ == request_timestamp1);
+  CHECK(rr[0].response_times_[0] == response_timestamp1);
+  CHECK(rr[0].response_times_[1] == response_timestamp2);
+  CHECK(rr[1].sequence_id_ == sequence_id2);
+  CHECK(rr[1].start_time_ == request_timestamp2);
+  CHECK(rr[1].response_times_[0] == response_timestamp3);
+  CHECK(rr[1].response_times_[1] == response_timestamp4);
+}
 
-    std::vector<RequestRecord> rr{(*it).requests};
-    CHECK(rr[0].sequence_id_ == sequence_id_0);
-    CHECK(rr[1].sequence_id_ == sequence_id_1);
-  }
+TEST_CASE("profile_data_collector: AddWindow")
+{
+  MockProfileDataCollector collector{};
+  InferenceLoadMode infer_mode{10, 20.0};
 
-  SUBCASE("AddWindow")
-  {
-    uint64_t window_start_ns_0{123};
-    uint64_t window_end_ns_0{456};
-    collector.AddWindow(infer_mode, window_start_ns_0, window_end_ns_0);
+  uint64_t window_start1{123};
+  uint64_t window_end1{456};
+  collector.AddWindow(infer_mode, window_start1, window_end1);
 
-    it = collector.FindExperiment(infer_mode);
-    CHECK(it != collector.experiments_.end());
-    CHECK((*it).window_boundaries[0] == window_start_ns_0);
-    CHECK((*it).window_boundaries[1] == window_end_ns_0);
+  CHECK(!collector.experiments_.empty());
+  CHECK(collector.experiments_[0].window_boundaries[0] == window_start1);
+  CHECK(collector.experiments_[0].window_boundaries[1] == window_end1);
 
-    uint64_t window_start_ns_1{678};
-    uint64_t window_end_ns_1{912};
-    collector.AddWindow(infer_mode, window_start_ns_1, window_end_ns_1);
+  uint64_t window_start2{678};
+  uint64_t window_end2{912};
+  collector.AddWindow(infer_mode, window_start2, window_end2);
 
-    it = collector.FindExperiment(infer_mode);
-    CHECK(it != collector.experiments_.end());
-    CHECK((*it).window_boundaries[2] == window_start_ns_1);
-    CHECK((*it).window_boundaries[3] == window_end_ns_1);
-  }
+  CHECK(collector.experiments_[0].window_boundaries[2] == window_start2);
+  CHECK(collector.experiments_[0].window_boundaries[3] == window_end2);
 }
 
 }}  // namespace triton::perfanalyzer
--- src/c++/perf_analyzer/test_profile_data_exporter.cc
+++ src/c++/perf_analyzer/test_profile_data_exporter.cc
@@ -30,42 +30,40 @@
 
 namespace triton { namespace perfanalyzer {
 
-TEST_CASE("profile_data_exporter")
+TEST_CASE("profile_data_exporter: ConvertToJson")
 {
   MockProfileDataExporter exporter{};
 
-  SUBCASE("ConvertToJson")
-  {
-    InferenceLoadMode infer_mode{4, 0.0};
-    uint64_t sequence_id{1};
+  InferenceLoadMode infer_mode{4, 0.0};
+  uint64_t sequence_id{1};
 
-    auto clock_epoch{std::chrono::time_point<std::chrono::system_clock>()};
-    auto request_timestamp{clock_epoch + std::chrono::nanoseconds(1)};
-    auto response_timestamp1{clock_epoch + std::chrono::nanoseconds(2)};
-    auto response_timestamp2{clock_epoch + std::chrono::nanoseconds(3)};
+  auto clock_epoch{std::chrono::time_point<std::chrono::system_clock>()};
+  auto request_timestamp{clock_epoch + std::chrono::nanoseconds(1)};
+  auto response_timestamp1{clock_epoch + std::chrono::nanoseconds(2)};
+  auto response_timestamp2{clock_epoch + std::chrono::nanoseconds(3)};
 
-    RequestRecord request_record{
-        request_timestamp,
-        std::vector<std::chrono::time_point<std::chrono::system_clock>>{
-            response_timestamp1, response_timestamp2},
-        0,
-        false,
-        sequence_id,
-        false};
-    std::vector<RequestRecord> requests{request_record};
-    std::vector<uint64_t> window_boundaries{1, 5, 6};
+  RequestRecord request_record{
+      request_timestamp,
+      std::vector<std::chrono::time_point<std::chrono::system_clock>>{
+          response_timestamp1, response_timestamp2},
+      0,
+      false,
+      sequence_id,
+      false};
+  std::vector<RequestRecord> requests{request_record};
+  std::vector<uint64_t> window_boundaries{1, 5, 6};
 
-    Experiment experiment;
-    experiment.mode = infer_mode;
-    experiment.requests = requests;
-    experiment.window_boundaries = window_boundaries;
-    std::vector<Experiment> experiments{experiment};
+  Experiment experiment;
+  experiment.mode = infer_mode;
+  experiment.requests = requests;
+  experiment.window_boundaries = window_boundaries;
+  std::vector<Experiment> experiments{experiment};
 
-    std::string version{"1.2.3"};
+  std::string version{"1.2.3"};
 
-    exporter.ConvertToJson(experiments, version);
+  exporter.ConvertToJson(experiments, version);
 
-    std::string json{R"(
+  std::string json{R"(
       {
         "experiments" : [
           {
@@ -87,95 +85,98 @@ TEST_CASE("profile_data_exporter")
       }
       )"};
 
-    rapidjson::Document expected_document;
-    expected_document.Parse(json.c_str());
+  rapidjson::Document expected_document;
+  expected_document.Parse(json.c_str());
 
-    const rapidjson::Value& expected_experiment{
-        expected_document["experiments"][0]["experiment"]};
-    const rapidjson::Value& expected_request{
-        expected_document["experiments"][0]["requests"][0]};
-    const rapidjson::Value& expected_windows{
-        expected_document["experiments"][0]["window_boundaries"]};
-    const rapidjson::Value& expected_version{expected_document["version"]};
+  const rapidjson::Value& expected_experiment{
+      expected_document["experiments"][0]["experiment"]};
+  const rapidjson::Value& expected_request{
+      expected_document["experiments"][0]["requests"][0]};
+  const rapidjson::Value& expected_windows{
+      expected_document["experiments"][0]["window_boundaries"]};
+  const rapidjson::Value& expected_version{expected_document["version"]};
 
-    const rapidjson::Value& actual_experiment{
-        exporter.document_["experiments"][0]["experiment"]};
-    const rapidjson::Value& actual_request{
-        exporter.document_["experiments"][0]["requests"][0]};
-    const rapidjson::Value& actual_windows{
-        exporter.document_["experiments"][0]["window_boundaries"]};
-    const rapidjson::Value& actual_version{exporter.document_["version"]};
+  const rapidjson::Value& actual_experiment{
+      exporter.document_["experiments"][0]["experiment"]};
+  const rapidjson::Value& actual_request{
+      exporter.document_["experiments"][0]["requests"][0]};
+  const rapidjson::Value& actual_windows{
+      exporter.document_["experiments"][0]["window_boundaries"]};
+  const rapidjson::Value& actual_version{exporter.document_["version"]};
 
-    CHECK(actual_experiment["mode"] == expected_experiment["mode"]);
-    CHECK(actual_experiment["value"] == expected_experiment["value"]);
+  CHECK(actual_experiment["mode"] == expected_experiment["mode"]);
+  CHECK(actual_experiment["value"] == expected_experiment["value"]);
 
-    CHECK(actual_request["timestamp"] == expected_request["timestamp"]);
-    CHECK(actual_request["sequence_id"] == expected_request["sequence_id"]);
-    CHECK(
-        actual_request["response_timestamps"][0] ==
-        expected_request["response_timestamps"][0]);
-    CHECK(
-        actual_request["response_timestamps"][1] ==
-        expected_request["response_timestamps"][1]);
+  CHECK(actual_request["timestamp"] == expected_request["timestamp"]);
+  CHECK(actual_request["sequence_id"] == expected_request["sequence_id"]);
+  CHECK(
+      actual_request["response_timestamps"][0] ==
+      expected_request["response_timestamps"][0]);
+  CHECK(
+      actual_request["response_timestamps"][1] ==
+      expected_request["response_timestamps"][1]);
 
-    CHECK(actual_windows[0] == expected_windows[0]);
-    CHECK(actual_windows[1] == expected_windows[1]);
-    CHECK(actual_windows[2] == expected_windows[2]);
+  CHECK(actual_windows[0] == expected_windows[0]);
+  CHECK(actual_windows[1] == expected_windows[1]);
+  CHECK(actual_windows[2] == expected_windows[2]);
 
-    CHECK(actual_version == expected_version);
+  CHECK(actual_version == expected_version);
+}
+
+TEST_CASE("profile_data_exporter: AddExperiment")
+{
+  MockProfileDataExporter exporter{};
+
+  Experiment raw_experiment;
+  rapidjson::Value entry(rapidjson::kObjectType);
+  rapidjson::Value experiment(rapidjson::kObjectType);
+
+  SUBCASE("Concurrency mode")
+  {
+    InferenceLoadMode infer_mode{15, 0.0};
+    raw_experiment.mode = infer_mode;
+
+    exporter.AddExperiment(entry, experiment, raw_experiment);
+    CHECK(entry.HasMember("experiment"));
+    CHECK(entry["experiment"]["mode"] == "concurrency");
+    CHECK(entry["experiment"]["value"] == 15);
   }
 
-  SUBCASE("AddExperiment")
+  SUBCASE("Request rate mode")
   {
-    Experiment raw_experiment;
-    rapidjson::Value entry(rapidjson::kObjectType);
-    rapidjson::Value experiment(rapidjson::kObjectType);
+    InferenceLoadMode infer_mode{0, 23.0};
+    raw_experiment.mode = infer_mode;
 
-    SUBCASE("Concurrency mode")
-    {
-      InferenceLoadMode infer_mode{15, 0.0};
-      raw_experiment.mode = infer_mode;
-
-      exporter.AddExperiment(entry, experiment, raw_experiment);
-      CHECK(entry.HasMember("experiment"));
-      CHECK(entry["experiment"]["mode"] == "concurrency");
-      CHECK(entry["experiment"]["value"] == 15);
-    }
-
-    SUBCASE("Request rate mode")
-    {
-      InferenceLoadMode infer_mode{0, 23.0};
-      raw_experiment.mode = infer_mode;
-
-      exporter.AddExperiment(entry, experiment, raw_experiment);
-      CHECK(entry.HasMember("experiment"));
-      CHECK(entry["experiment"]["mode"] == "request_rate");
-      CHECK(entry["experiment"]["value"] == 23);
-    }
-  }
-
-  SUBCASE("OutputToFile")
-  {
-    std::string file_path;
-
-    SUBCASE("Empty file path")
-    {
-      file_path = "";
-      CHECK_THROWS_WITH_AS(
-          exporter.OutputToFile(file_path),
-          "failed to open file for outputting raw profile data",
-          PerfAnalyzerException);
-    }
-
-    SUBCASE("With file path")
-    {
-      file_path = "/tmp/test-" + GetRandomString(4) + ".json";
-      CHECK_NOTHROW(exporter.OutputToFile(file_path));
-      CHECK(IsFile(file_path));
-
-      std::remove(file_path.c_str());
-      CHECK(!IsFile(file_path));
-    }
+    exporter.AddExperiment(entry, experiment, raw_experiment);
+    CHECK(entry.HasMember("experiment"));
+    CHECK(entry["experiment"]["mode"] == "request_rate");
+    CHECK(entry["experiment"]["value"] == 23);
   }
 }
+
+TEST_CASE("profile_data_exporter: OutputToFile")
+{
+  MockProfileDataExporter exporter{};
+  std::string file_path;
+
+  SUBCASE("Empty file path")
+  {
+    file_path = "";
+    CHECK_THROWS_WITH_AS(
+        exporter.OutputToFile(file_path),
+        "failed to open file for outputting raw profile data",
+        PerfAnalyzerException);
+  }
+
+  SUBCASE("With file path")
+  {
+    file_path = "/tmp/test-" + GetRandomString(4) + ".json";
+    CHECK_NOTHROW(exporter.OutputToFile(file_path));
+    CHECK(IsFile(file_path));
+
+    std::remove(file_path.c_str());
+    CHECK(!IsFile(file_path));
+  }
+}
+
 }}  // namespace triton::perfanalyzer
