1e6200ed89e8d4bfd3b6a36d3b512b0bcc91bf49
Matthew Kotila <matthew.r.kotila@gmail.com>
2023-09-19 14:51:48 +0000

    Fix CodeQL issue

--- src/c++/perf_analyzer/docs/examples/calculate_avg_first_token_latency.py
+++ src/c++/perf_analyzer/docs/examples/calculate_avg_first_token_latency.py
@@ -1,11 +1,10 @@
 import json
 
-f = open("profile_export.json")
+with open("profile_export.json") as f:
+    # example json demonstrating format:
+    # https://github.com/triton-inference-server/client/blob/main/src/c%2B%2B/perf_analyzer/docs/examples/decoupled_output_file.json
+    requests = json.load(f)["experiments"][0]["requests"]
+    latencies = [r["response_timestamps"][0] - r["timestamp"] for r in requests]
+    avg_latency_s = sum(latencies) / len(latencies) / 1000000000
 
-# example json demonstrating format:
-# https://github.com/triton-inference-server/client/blob/main/src/c%2B%2B/perf_analyzer/docs/examples/decoupled_output_file.json
-requests = json.load(f)["experiments"][0]["requests"]
-latencies = [r["response_timestamps"][0] - r["timestamp"] for r in requests]
-avg_latency_s = sum(latencies) / len(latencies) / 1000000000
-
-print("Average first-token latency: " + str(avg_latency_s) + " s")
+    print("Average first-token latency: " + str(avg_latency_s) + " s")
--- src/c++/perf_analyzer/docs/examples/calculate_avg_token_to_token_latency.py
+++ src/c++/perf_analyzer/docs/examples/calculate_avg_token_to_token_latency.py
@@ -1,16 +1,15 @@
 import json
 
-f = open("profile_export.json")
+with open("profile_export.json") as f:
+    # example json demonstrating format:
+    # https://github.com/triton-inference-server/client/blob/main/src/c%2B%2B/perf_analyzer/docs/examples/decoupled_output_file.json
+    requests = json.load(f)["experiments"][0]["requests"]
+    latencies = []
+    for request in requests:
+        prev_response = request["response_timestamps"][0]
+        for response in request["response_timestamps"][1:]:
+            latencies.append(response - prev_response)
+            prev_response = response
+    avg_latency_s = sum(latencies) / len(latencies) / 1000000000
 
-# example json demonstrating format:
-# https://github.com/triton-inference-server/client/blob/main/src/c%2B%2B/perf_analyzer/docs/examples/decoupled_output_file.json
-requests = json.load(f)["experiments"][0]["requests"]
-latencies = []
-for request in requests:
-    prev_response = request["response_timestamps"][0]
-    for response in request["response_timestamps"][1:]:
-        latencies.append(response - prev_response)
-        prev_response = response
-avg_latency_s = sum(latencies) / len(latencies) / 1000000000
-
-print("Average token-to-token latency: " + str(avg_latency_s) + " s")
+    print("Average token-to-token latency: " + str(avg_latency_s) + " s")
