eb32423260822e16191c7ab2f81ea1fdf6fea1c4
Hyunjae Woo <hwoo@nvidia.com>
2024-03-01 11:03:08 -0800

    Address feedback

--- src/c++/perf_analyzer/genai-pa/genai_pa/llm_profile.py
+++ src/c++/perf_analyzer/genai-pa/genai_pa/llm_profile.py
@@ -4,7 +4,8 @@
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
-# are met: * Redistributions of source code must retain the above copyright
+# are met:
+#  * Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
 #  * Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
@@ -25,21 +26,29 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import json
 from dataclasses import dataclass
 from itertools import pairwise
 
 import numpy as np
+from genai_pa.utils import load_json
 from transformers import AutoTokenizer
 
 
 @dataclass
 class LLMMetrics:
+    """A simple dataclass that holds core LLM performance metrics."""
+
     time_to_first_tokens: list[int]
     inter_token_latencies: list[int]
     output_token_throughputs: list[int]
 
     def get_base_name(self, attr_name: str) -> str:
+        # Attempted to extract and store the mapping as a dataclass member as a
+        # dictionary but encountered two issues: (1) Python does not allow
+        # dataclass member to be mutable and (2) if we set it as member of
+        # normal class, the dict member will be parsed by Statistics class,
+        # which is not what we want since it's not one of the LLM metrics.
+        # Leaving it as conditional statements for now.
         if attr_name == "time_to_first_tokens":
             return "time_to_first_token"
         elif attr_name == "inter_token_latencies":
@@ -51,15 +60,32 @@ class LLMMetrics:
 
 
 class Statistics:
-    # TODO: make this parameter LLM agnostic
+    """A class that aggregates various statistics from given metrics class.
+
+    The Statistics class goes through each metric in the metrics class and
+    calculates several statistics such as:
+      - average (arithmetic mean)
+      - percentiles (p50, p90, p95, p99)
+      - minimum & maximum
+      - standard deviation
+    The class will store each calculated statistics as part of its attribute.
+
+    Example:
+
+      >>> metrics = LLMMetrics([3, 4, 5], [], [])
+      >>> stats = Statistics(metrics)
+      >>> print(stats.avg_time_to_first_token)  # output: 4
+    """
+
     def __init__(self, metrics: LLMMetrics):
         # iterate through LLMMetrics to calculate statistics and set attributes
         for attr, data in metrics.__dict__.items():
-            attr = metrics.get_base_name(attr)
-            self._calculate_mean(data, attr)
-            self._calculate_percentiles(data, attr)
-            self._calculate_minmax(data, attr)
-            self._calculate_std(data, attr)
+            if data:
+                attr = metrics.get_base_name(attr)
+                self._calculate_mean(data, attr)
+                self._calculate_percentiles(data, attr)
+                self._calculate_minmax(data, attr)
+                self._calculate_std(data, attr)
 
     def _calculate_mean(self, data: list[int], attr: str):
         avg = np.mean(data)
@@ -82,33 +108,46 @@ class Statistics:
         setattr(self, "std_" + attr, std)
 
     def __repr__(self):
-        attr_str = ""
-        for k, v in self.__dict__.items():
-            attr_str += f"{k}={v},"
-        return f"Statistics({attr_str})"
+        attr_strs = ",".join([f"{k}={v}" for k, v in self.__dict__.items()])
+        return f"Statistics({attr_strs})"
 
 
 class LLMProfileData:
-    def __init__(self, filename: str, tokenizer_model: str = "gpt2") -> None:
-        # load profile export data
-        with open(filename) as f:
-            data = json.load(f)
+    """A class that calculates and aggregates all the LLM performance statistics
+    across the Perf Analyzer profile results.
 
-        tokenizer = AutoTokenizer.from_pretrained(tokenizer_model)
+    The LLMProfileData class parses profile export JSON file, collects the core
+    LLM performance metrics, and calculates summary statistics for each different
+    Perf Analyzer runs/experiments.
 
+    Example:
+
+      >>> ... # run Perf Analyzer with concurrency level 10
+      >>>
+      >>> from transformers import AutoTokenizer
+      >>>
+      >>> tokenizer = AutoTokenizer.from_pretrained("gpt2")
+      >>> pd = LLMProfileData(filename="profile_export.json", tokenizer)
+      >>> stats = pd.get_statistics(infer_mode="concurrency", level=10)
+      >>>
+      >>> print(stats)  # output: Statistics(avg_time_to_first_token=...)
+    """
+
+    def __init__(self, filename: str, tokenizer: AutoTokenizer) -> None:
+        data = load_json(filename)
         self._profile_results = {}
+
         for experiment in data["experiments"]:
             infer_mode = experiment["experiment"]["mode"]
-            level = experiment["experiment"]["value"]
+            load_level = experiment["experiment"]["value"]
             requests = experiment["requests"]
 
             metrics = self._collect_llm_metrics(requests, tokenizer)
 
             # aggregate and calculate statistics
             statistics = Statistics(metrics)
-            self._profile_results[(infer_mode, level)] = statistics
+            self._profile_results[(infer_mode, load_level)] = statistics
 
-    # TODO: handle single response case
     def _collect_llm_metrics(
         self, requests: dict, tokenizer: AutoTokenizer
     ) -> LLMMetrics:
@@ -125,10 +164,9 @@ class LLMProfileData:
 
             # output token throughput
             output_tokens = tokenizer(res_outputs)["input_ids"]
-            num_output_tokens = list(map(lambda x: len(x), output_tokens))
-            total_output_tokens = np.sum(num_output_tokens)
-            output_latency = res_timestamps[-1] - res_timestamps[0]
-            output_token_throughputs.append(total_output_tokens / output_latency)
+            total_output_tokens = np.sum(list(map(len, output_tokens)))
+            req_latency = res_timestamps[-1] - req_timestamp
+            output_token_throughputs.append(total_output_tokens / req_latency)
 
             # inter token latency
             for t1, t2 in pairwise(res_timestamps):
@@ -140,7 +178,7 @@ class LLMProfileData:
             output_token_throughputs,
         )
 
-    def get_statistics(self, infer_mode: str, level: int | float) -> Statistics:
-        if (infer_mode, level) not in self._profile_results:
-            raise KeyError(f"Profile with {infer_mode}={level} does not exist.")
-        return self._profile_results[(infer_mode, level)]
+    def get_statistics(self, infer_mode: str, load_level: int | float) -> Statistics:
+        if (infer_mode, load_level) not in self._profile_results:
+            raise KeyError(f"Profile with {infer_mode}={load_level} does not exist.")
+        return self._profile_results[(infer_mode, load_level)]
