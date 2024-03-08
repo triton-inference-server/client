27d3eb78c1d6c14695a8e53d50edbd027fb6dfab
tgerdes <tgerdes@nvidia.com>
2024-03-08 07:33:48 -0600

    Workaround: Remove non-utf at start of triton response string

--- src/c++/perf_analyzer/genai-pa/genai_pa/llm_metrics.py
+++ src/c++/perf_analyzer/genai-pa/genai_pa/llm_metrics.py
@@ -372,9 +372,24 @@ class LLMProfileDataParser(ProfileDataParser):
             num_generated_tokens,
         )
 
+    def _remove_leading_invalid_chars(self, text: str):
+        for i, char in enumerate(text):
+            # There will be 3 or 4 chars 
+            # (but sometimes the first char looks valid, so don't stop until we've seen at least 3)
+            if char.isprintable() and i > 2:
+                break
+
+        return text[i:]
+
     def _preprocess_response(
         self, res_timestamps: list[int], res_outputs: list[dict[str, str]]
     ) -> None:
+        # FIXME -- remove this triton code once it is properly fixed in PA
+        # (PA/triton will add junk to the start of the BYTES array. Remove it here)
+        if self._service_kind == "triton":
+            for d in res_outputs:
+                d['text_output'] = self._remove_leading_invalid_chars(d['text_output'])
+
         """Helper function to preprocess responses of a request."""
         if self._service_kind == "openai":
             openai_final_response = res_outputs[-1]["response"]
