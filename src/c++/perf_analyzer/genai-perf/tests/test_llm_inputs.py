ea34edfcc3ba99e55bd65d0e395f66019f42b28d
David Yastremsky <dyastremsky@nvidia.com>
2024-04-09 17:07:25 -0700

    Add test for output token distribution

--- src/c++/perf_analyzer/genai-perf/tests/test_llm_inputs.py
+++ src/c++/perf_analyzer/genai-perf/tests/test_llm_inputs.py
@@ -23,6 +23,14 @@ from genai_perf.llm_inputs.llm_inputs import LlmInputs, OutputFormat, PromptSour
 
 
 class TestLlmInputs:
+    # Define service kind, backend or endpoint, and output format combinations
+    SERVICE_KIND_BACKEND_ENDPOINT_FORMATS = [
+        ("triton", "vllm", OutputFormat.VLLM),
+        ("triton", "trtllm", OutputFormat.TRTLLM),
+        ("openai", "v1/completions", OutputFormat.OPENAI_COMPLETIONS),
+        ("openai", "v1/chat/completions", OutputFormat.OPENAI_CHAT_COMPLETIONS),
+    ]
+
     @pytest.fixture
     def default_configured_url(self):
         default_configured_url = LlmInputs._create_configured_url(
@@ -277,17 +285,18 @@ class TestLlmInputs:
             LlmInputs.DEFAULT_RANDOM_SEED,
         )
 
-        # 550 is the num of tokens returned for the default seed
         token_length = len(default_tokenizer.encode(synthetic_prompt))
         assert token_length == 550
 
-        synthetic_prompt, synthetic_prompt_tokens = LlmInputs._create_synthetic_prompt(
+        synthetic_prompt = LlmInputs._create_synthetic_prompt(
             default_tokenizer,
             LlmInputs.DEFAULT_PROMPT_TOKENS_MEAN,
             LlmInputs.DEFAULT_PROMPT_TOKENS_STDDEV + 250,
             LlmInputs.DEFAULT_RANDOM_SEED + 1,
         )
-        assert synthetic_prompt_tokens != 785
+
+        token_length = len(default_tokenizer.encode(synthetic_prompt))
+        assert token_length != 785
 
     def test_synthetic_to_vllm(self, default_tokenizer):
         """
@@ -362,13 +371,7 @@ class TestLlmInputs:
         assert len(pa_json["data"]) == 5
 
     @pytest.mark.parametrize(
-        "output_format",
-        [
-            (OutputFormat.OPENAI_CHAT_COMPLETIONS),
-            (OutputFormat.OPENAI_COMPLETIONS),
-            (OutputFormat.TRTLLM),
-            (OutputFormat.VLLM),
-        ],
+        "output_format", [format[2] for format in SERVICE_KIND_BACKEND_ENDPOINT_FORMATS]
     )
     def test_extra_inputs(self, default_tokenizer, output_format) -> None:
         input_name = "max_tokens"
@@ -433,3 +436,83 @@ class TestLlmInputs:
             assert entry[input_name] == [
                 input_value
             ], f"The value of {input_name} is incorrect"
+
+    @pytest.mark.parametrize(
+        "output_format", [format[2] for format in SERVICE_KIND_BACKEND_ENDPOINT_FORMATS]
+    )
+    def test_output_tokens_mean(self, output_format, default_tokenizer):
+        output_tokens_mean = 100
+        output_tokens_stddev = 0
+
+        pa_json = LlmInputs.create_llm_inputs(
+            input_type=PromptSource.SYNTHETIC,
+            output_format=output_format,
+            num_of_output_prompts=5,
+            add_model_name=False,
+            add_stream=True,
+            tokenizer=default_tokenizer,
+            output_tokens_mean=output_tokens_mean,
+            output_tokens_stddev=output_tokens_stddev,
+        )
+
+        assert os.path.exists(
+            DEFAULT_INPUT_DATA_JSON
+        ), "llm_inputs.json file is not created"
+
+        with open(DEFAULT_INPUT_DATA_JSON, "r") as f:
+            llm_inputs_data = json.load(f)
+
+        item = None
+        for entry in llm_inputs_data["data"]:
+            if (
+                output_format == OutputFormat.OPENAI_COMPLETIONS
+                or output_format == OutputFormat.OPENAI_CHAT_COMPLETIONS
+            ):
+                assert "payload" in entry, "payload is missing in llm_inputs.json"
+                payload = entry["payload"][0]
+                assert (
+                    "max_tokens" in payload
+                ), "max_tokens parameter is missing in llm_inputs.json"
+                assert (
+                    payload["max_tokens"] == output_tokens_mean
+                ), "max_tokens parameter is not properly set"
+                assert (
+                    "ignore_eos" in payload
+                ), "ignore_eos parameter is missing in llm_inputs.json"
+                assert (
+                    payload["ignore_eos"] == True
+                ), "ignore_eos parameter is not properly set"
+            elif output_format == OutputFormat.VLLM:
+                assert (
+                    "sampling_parameters" in entry
+                ), "sampling_parameters is missing in llm_inputs.json"
+                sampling_parameters = json.loads(entry["sampling_parameters"][0])
+                assert (
+                    "max_tokens" in sampling_parameters
+                ), "max_tokens parameter is missing in sampling_parameters"
+                assert sampling_parameters["max_tokens"] == str(
+                    output_tokens_mean
+                ), "max_tokens parameter is not properly set"
+                assert (
+                    "ignore_eos" in sampling_parameters
+                ), "ignore_eos parameter is missing in sampling_parameters"
+                assert sampling_parameters["ignore_eos"] == str(
+                    True
+                ), "ignore_eos parameter is not properly set"
+            elif output_format == OutputFormat.TRTLLM:
+                assert (
+                    "max_tokens" in entry
+                ), "max_tokens parameter is missing in llm_inputs.json"
+                assert (
+                    entry["max_tokens"][0] == output_tokens_mean
+                ), "max_tokens parameter is not properly set"
+                assert (
+                    "ignore_eos" in entry
+                ), "ignore_eos parameter is missing in llm_inputs.json"
+                assert (
+                    entry["ignore_eos"][0] == True
+                ), "ignore_eos parameter is not properly set"
+            else:
+                assert False, f"Unsupported output format: {output_format}"
+
+        os.remove(DEFAULT_INPUT_DATA_JSON)
