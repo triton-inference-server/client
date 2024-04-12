13347b3f7752c87b2219a9c0fb7146d548982e27
David Yastremsky <dyastremsky@nvidia.com>
2024-04-12 11:48:37 -0700

    Remove unused imports

--- src/c++/perf_analyzer/genai-perf/genai_perf/llm_metrics.py
+++ src/c++/perf_analyzer/genai-perf/genai_perf/llm_metrics.py
@@ -26,9 +26,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import contextlib
 import csv
-import io
 import json
 from itertools import pairwise
 from typing import List
