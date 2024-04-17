94ebf2e11398f9c73f34a2cc436652e1478a4b09
David Yastremsky <dyastremsky@nvidia.com>
2024-04-17 15:33:27 -0700

    Fix missing arg

--- src/c++/perf_analyzer/perf_analyzer.cc
+++ src/c++/perf_analyzer/perf_analyzer.cc
@@ -78,11 +78,11 @@ PerfAnalyzer::CreateAnalyzerObjects()
   FAIL_IF_ERR(
       cb::ClientBackendFactory::Create(
           params_->kind, params_->url, params_->endpoint, params_->protocol,
-          params_->compression_algorithm, params_->http_headers,
-          params_->triton_server_path, params_->model_repository_path,
-          params_->extra_verbose, params_->metrics_url,
-          params_->input_tensor_format, params_->output_tensor_format,
-          &factory),
+          params_->ssl_options, params_->compression_algorithm,
+          params_->http_headers, params_->triton_server_path,
+          params_->model_repository_path, params_->extra_verbose,
+          params_->metrics_url, params_->input_tensor_format,
+          params_->output_tensor_format, &factory),
       "failed to create client factory");
 
   FAIL_IF_ERR(
