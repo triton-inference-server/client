c49a85fbd5f50548afbbbd301a72097d2a5fff5f
Timothy Gerdes <50968584+tgerdesnv@users.noreply.github.com>
2024-05-02 09:53:13 -0500

    Fix input token calculation (#625)
    * Fix issue where wrong data was used for calculating input tokens
    
    * Fix issue where input token calculation was different between creation and metrics calculation
    
    * update unit tests

--- src/c++/perf_analyzer/genai-perf/genai_perf/llm_metrics.py
+++ src/c++/perf_analyzer/genai-perf/genai_perf/llm_metrics.py
@@ -464,12 +464,6 @@ class LLMProfileDataParser(ProfileDataParser):
         tokenizer: Tokenizer,
     ) -> None:
         self._tokenizer = tokenizer
-        # Disable add_bos_token so that llama tokenizer does not add bos token
-        # (aka. beginning-of-sentence) to the beginning of every response
-        # outputs, increasing the token count by 1 for each output response.
-        # Note: The type is being ignored here, because not all tokenizers have
-        # an add_bos_token variable.
-        self._tokenizer.add_bos_token = False  # type: ignore
         self._service_kind = service_kind
         self._output_format = output_format
         super().__init__(filename)
@@ -587,7 +581,7 @@ class LLMProfileDataParser(ProfileDataParser):
         if self._output_format == _OPENAI_CHAT_COMPLETIONS:
             input_text = payload["messages"][0]["content"]
         elif self._output_format == _OPENAI_COMPLETIONS:
-            input_text = payload["prompt"][0]
+            input_text = payload["prompt"]
         else:
             raise ValueError(
                 "Failed to parse OpenAI request input in profile export file."
