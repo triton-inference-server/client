3daf2e3d9942af969e608fddd107112212a84fe8
Ganesh Kudleppanavar <158088791+ganeshku1@users.noreply.github.com>
2024-05-10 19:27:46 -0500

    Revert "Changes to support Ensemble Top Level Response Caching (#560) (#642)"
    This reverts commit cc6a3b27b7d281079e6baa2f2ad7bb0362ed79fe.

--- src/c++/perf_analyzer/test_inference_profiler.cc
+++ src/c++/perf_analyzer/test_inference_profiler.cc
@@ -160,14 +160,7 @@ class TestInferenceProfiler : public InferenceProfiler {
     return InferenceProfiler::DetermineStatsModelVersion(
         model_identifier, start_stats, end_stats, model_version);
   }
-
-  cb::Error SetTopLevelResponseCaching(bool enable_top_level_response_caching)
-  {
-    return InferenceProfiler::SetTopLevelResponseCaching(
-        enable_top_level_response_caching);
-  }
 };
-
 
 TEST_CASE("testing the ValidLatencyMeasurement function")
 {
@@ -857,25 +850,6 @@ TEST_CASE("determine_stats_model_version: testing DetermineStatsModelVersion()")
     expect_exception = true;
   }
 
-  SUBCASE("One entry - version -1 - valid and in start")
-  {
-    model_identifier = {"ModelA", "-1"};
-    start_stats_map.insert({{"ModelA", "3"}, old_stats});
-    end_stats_map.insert({{"ModelA", "3"}, new_stats});
-    cb::Error status = tip.SetTopLevelResponseCaching(true);
-    CHECK(status.IsOk());
-    expected_model_version = -1;
-  }
-
-  SUBCASE("One entry - version -1 - not valid")
-  {
-    model_identifier = {"ModelA", "-1"};
-    end_stats_map.insert({{"ModelA", "3"}, old_stats});
-    cb::Error status = tip.SetTopLevelResponseCaching(false);
-    CHECK(status.IsOk());
-    expected_model_version = -1;
-    expect_exception = true;
-  }
 
   std::stringstream captured_cerr;
   std::streambuf* old = std::cerr.rdbuf(captured_cerr.rdbuf());
