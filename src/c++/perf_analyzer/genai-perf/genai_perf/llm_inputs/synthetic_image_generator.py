9f8a42677ea684332d18f1d127944214ebb82b0e
Marek Wawrzos <mwawrzos@nvidia.com>
2024-07-11 16:27:48 +0200

    prepare SyntheticImageGenerator to support different image sources

--- src/c++/perf_analyzer/genai-perf/genai_perf/llm_inputs/synthetic_image_generator.py
+++ src/c++/perf_analyzer/genai-perf/genai_perf/llm_inputs/synthetic_image_generator.py
@@ -1,6 +1,7 @@
 import base64
 from enum import Enum, auto
 from io import BytesIO
+from typing import Tuple
 
 import numpy as np
 from genai_perf.exceptions import GenAIPerfException
@@ -26,13 +27,30 @@ class RandomFormatBase64Encoder:
         return f"{prefix},{data}"
 
 
+def white_images_generator():
+    white_image = Image.new("RGB", (100, 100), color="white")
+    while True:
+        yield white_image
+
+
+def build_synthetic_image_generator(
+    mean_size: Tuple[int, int], dimensions_stddev: Tuple[int, int]
+):
+    return SyntheticImageGenerator(
+        mean_size=mean_size,
+        dimensions_stddev=dimensions_stddev,
+        image_iterator=white_images_generator(),
+    )
+
+
 class SyntheticImageGenerator:
     def __init__(
         self,
         mean_size,
         dimensions_stddev,
+        image_iterator,
     ):
-        self._image_iterator = self.white_images_iterator()
+        self.image_iterator = image_iterator
         self.mean_size = mean_size
         self.dimensions_stddev = dimensions_stddev
 
@@ -45,11 +63,6 @@ class SyntheticImageGenerator:
         return image.resize(new_size)
 
     def __next__(self):
-        image = next(self._image_iterator)
+        image = next(self.image_iterator)
         image = self.random_resize(image)
         return image
-
-    def white_images_iterator(self):
-        white_image = Image.new("RGB", (100, 100), color="white")
-        while True:
-            yield white_image
--- src/c++/perf_analyzer/genai-perf/tests/test_synthetic_image_generator.py
+++ src/c++/perf_analyzer/genai-perf/tests/test_synthetic_image_generator.py
@@ -1,11 +1,13 @@
 import base64
 from io import BytesIO
 
+import numpy as np
 import pytest
 from genai_perf.llm_inputs.synthetic_image_generator import (
     ImageFormat,
     RandomFormatBase64Encoder,
-    SyntheticImageGenerator,
+    build_synthetic_image_generator,
+    white_images_generator,
 )
 from PIL import Image
 
@@ -18,12 +20,23 @@ from PIL import Image
     ],
 )
 def test_different_image_size(image_size):
-    sut = SyntheticImageGenerator(mean_size=image_size, dimensions_stddev=[0, 0])
+    sut = build_synthetic_image_generator(
+        mean_size=image_size, dimensions_stddev=[0, 0]
+    )
 
     image = next(sut)
 
     assert isinstance(image, Image.Image), "generator produces unexpected type of data"
     assert image.size == image_size, "image not resized to the target size"
+
+
+def test_white_images_generator():
+    sut = white_images_generator()
+
+    image = next(sut)
+    assert isinstance(image, Image.Image), "generator produces unexpected type of data"
+    white_pixel = np.array([[[255, 255, 255]]])
+    assert (np.array(image) == white_pixel).all(), "not all pixels are white"
 
 
 @pytest.mark.parametrize("image_format", [ImageFormat.PNG, ImageFormat.JPEG])
