af0a93a88ba00c3844b0b0609560ab128ca43492
Marek Wawrzos <mwawrzos@nvidia.com>
2024-07-15 14:27:23 +0200

    SyntheticImageGenerator doesn't load files

--- src/c++/perf_analyzer/genai-perf/genai_perf/llm_inputs/synthetic_image_generator.py
+++ src/c++/perf_analyzer/genai-perf/genai_perf/llm_inputs/synthetic_image_generator.py
@@ -2,7 +2,7 @@ import base64
 from enum import Enum, auto
 from io import BytesIO
 from pathlib import Path
-from typing import Generator, List, Optional, Tuple, cast
+from typing import Generator, Optional, Tuple, cast
 
 import numpy as np
 from genai_perf.exceptions import GenAIPerfException
@@ -25,46 +25,6 @@ class Base64Encoder:
         return f"data:image/{self.image_format.name.lower()};base64,{data}"
 
 
-def images_from_file_generator(image_path: Path) -> Generator[Image.Image, None, None]:
-    if not image_path.exists():
-        raise GenAIPerfException(f"File not found: {image_path}")
-
-    image = Image.open(image_path)
-    while True:
-        yield image
-
-
-def white_images_generator() -> Generator[Image.Image, None, None]:
-    white_image = Image.new("RGB", (100, 100), color="white")
-    while True:
-        yield white_image
-
-
-def build_synthetic_image_generator(
-    image_width_mean: int,
-    image_height_mean: int,
-    image_width_stddev: int,
-    image_height_stddev: int,
-    image_path: Optional[Path] = None,
-    image_format: ImageFormat = ImageFormat.PNG,
-):
-    if image_path is None:
-        image_iterator = white_images_generator()
-    else:
-        image_path = cast(Path, image_path)
-        image_iterator = images_from_file_generator(image_path)
-
-    image_generator = SyntheticImageGenerator(
-        image_width_mean=image_width_mean,
-        image_height_mean=image_height_mean,
-        image_width_stddev=image_width_stddev,
-        image_height_stddev=image_height_stddev,
-        image_iterator=image_iterator,
-    )
-    base64_encode = Base64Encoder(image_format)
-    return (base64_encode(image) for image in image_generator)
-
-
 class SyntheticImageGenerator:
     def __init__(
         self,
@@ -72,10 +32,8 @@ class SyntheticImageGenerator:
         image_height_mean: int,
         image_width_stddev: int,
         image_height_stddev: int,
-        image_iterator: Generator[Image.Image, None, None],
         rng: Optional[np.random.Generator] = None,
     ):
-        self.image_iterator = image_iterator
         self._image_width_mean = image_width_mean
         self._image_height_mean = image_height_mean
         self._image_width_stddev = image_width_stddev
@@ -101,7 +59,10 @@ class SyntheticImageGenerator:
         )
         return image.resize((width, height))
 
+    def get_next_image(self):
+        return Image.new("RGB", (100, 100), color="white")
+
     def __next__(self):
-        image = next(self.image_iterator)
+        image = self.get_next_image()
         image = self.random_resize(image)
         return image
