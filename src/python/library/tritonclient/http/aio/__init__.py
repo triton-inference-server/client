bf672162d6c3bb9df6dc4710c99d1906efca8cf8
Yingge He <157551214+yinggeh@users.noreply.github.com>
2026-01-13 15:12:58 -0800

    feat: Expose HTTP error for non-JSON format response (#874) (#875)

--- src/python/library/tritonclient/http/aio/__init__.py
+++ src/python/library/tritonclient/http/aio/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -71,7 +71,7 @@ async def _get_error(response):
             )
         except Exception as e:
             return InferenceServerException(
-                msg=f"an exception occurred in the client while decoding the response: {e}",
+                msg=f"an exception occurred in the client while decoding the response: {e}\nresponse: {body}",
                 status=str(response.status),
                 debug_details=body,
             )
